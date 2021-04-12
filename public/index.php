9926f1f85dbfdc3d7b2bcef7bf0531f840eaeb69
ITReviewChannel <admin@itreviewchannel.ru>
2021-04-12 20:29:50 +0400

    Обновлена главная страница.

--- public/index.php
+++ public/index.php
@@ -2,4 +2,12 @@
 
 require '../vendor/autoload.php';
 
+echo 'Сайт заработал. Этот файл лежит тут: ';
+
+echo __FILE__;
+
+echo '<hr>';
+
+echo 'Пример расчета: 10 + 15 = ';
+
 echo (new \App\Calculator())->sum(10, 15);
